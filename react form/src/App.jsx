d381b1252c801b629cd1535321416759aeafb98b
Andre050101 <agiancristiano90@gmail.com>
2024-12-17 17:11:05 +0100

    Structure done, working on style

--- react form/src/App.jsx
+++ react form/src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
+import Form from './components/Form';
+import ArticleList from './components/ArticleList';
 
 function App() {
+  const [articles, setArticles] = useState([]);
 
+  //AddArticle
+  const addArticle = (article) => {
+    setArticles([...articles, article]);
+  };
+
+  //RemoveArticle
+  const removeArticle = (id) => {
+    setArticles(articles.filter((article) => article.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Handle Blog's Articles</h1>
+      <Form onAddArticle={addArticle} />
+      <ArticleList articles={articles} onRemoveArticle={removeArticle} />
+    </div>
+  );
 }
 
 export default App
