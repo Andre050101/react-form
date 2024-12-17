3f3b5d17e897b09d1f0c4f8af1db0d9b1cd042f8
Andre050101 <agiancristiano90@gmail.com>
2024-12-17 16:56:52 +0100

    form.jsx done

---
+++ react form/src/components/Form.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const Form = ({ onAddArticle }) => {
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [status, setStatus] = useState("draft");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!title.trim())
+            return alert("Title is required");
+        const newArticle = {
+            id: Date.now(),
+            title,
+            author,
+            status
+        };
+        onAddArticle(newArticle);
+        setTitle("");
+        setAuthor("");
+        setStatus("draft");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Title:</label>
+                <input
+                    type="text"
+                    name="title"
+                    onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <div>
+                <label>Author:</label>
+                <input
+                    type="text"
+                    name="author"
+                    onChange={(e) => setAuthor(e.target.value)} />
+            </div>
+            <div>
+                <label>Status:</label>
+                <select
+                    name="status"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                >
+                    <option value="draft">Draft</option>
+                    <option value="published">Published</option>
+                </select>
+            </div>
+            <button type="submit">Add Article</button>
+        </form>
+    );
+};
+
+export default Form;
